67d1d6552a7f2950a66d6916c65cb09945d24668
Clifford Pereira <cliffordpereira02@gmail.com>
2017-06-09 18:17:41 +0530

    msg

--- services/realTimeServices/updateRepository.js
+++ services/realTimeServices/updateRepository.js
@@ -55,7 +55,7 @@ function update(model){
                                                         "tags"  : model.data.tags     
                                                  }
                                     } 
-                    };
+                    };  
     
     var updateRequestParams     = {
                             url     : commonAccessUrl,
