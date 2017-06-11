106452f5826859b5eddb3642119d962eb4751d41
Clifford Pereira <cliffordpereira02@gmail.com>
2017-06-11 18:45:32 +0530

    msg

--- services/schedulerServices/awsApi.js
+++ services/schedulerServices/awsApi.js
@@ -86,7 +86,7 @@ function awsApiCall(model){
 //                            
 //                            global.emit("xmlRequestSetup",model)
 //                            model.emit("xmlRequestService",model)
-                            
+                                   
                            }
                         else{
                             commonVar.add()
