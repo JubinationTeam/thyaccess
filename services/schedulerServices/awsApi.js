36d316d97e4cd91b90702c6665ed359c13b17197
Clifford Pereira <cliffordpereira02@gmail.com>
2017-06-13 12:42:33 +0530

    msg

--- services/schedulerServices/awsApi.js
+++ services/schedulerServices/awsApi.js
@@ -67,8 +67,8 @@ function awsApiCall(model){
                                 model.aws=true;
                                 console.log("AWS link present")
                                  
-                                global.emit("readGuardSetUp",model)
-                                model.emit("readGuard",model)
+//                                global.emit("readGuardSetUp",model)
+//                                model.emit("readGuard",model)
                                                                 
                                 global.emit("xmlRequestSetup",model)
                                 model.emit("xmlRequestService",model)
